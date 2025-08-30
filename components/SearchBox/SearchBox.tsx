e930bf8845416dd16dc25896e052c47aff10e7f8
Олексій <alex.leon2010@icloud.com>
2025-08-30 18:00:41 +0300

    Initial commit

---
+++ components/SearchBox/SearchBox.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import React from "react";
+
+export interface SearchBoxProps {
+  value: string;
+  onSearch: (value: string) => void;
+}
+
+const SearchBox: React.FC<SearchBoxProps> = ({ value, onSearch }) => {
+  return (
+    <input
+      type="text"
+      value={value}
+      onChange={(e) => onSearch(e.target.value)} 
+      placeholder="Search notes..."
+      className="border px-3 py-2 rounded w-full"
+    />
+  );
+};
+
+export default SearchBox;
